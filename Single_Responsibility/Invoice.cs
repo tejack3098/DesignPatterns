753147f766deca0d93180a4c5707ec7e402d7c26
Tejas Choudhari <tejas.choudhari36@gmail.com>
2025-02-24 18:16:11 -0500

    Added SOLIDE PrIncples
    Added Single Responsibility demo

---
+++ Single_Responsibility/Invoice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Responsibility
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public decimal CalculateTotal() {
+            return Amount * 1.05m;
+        }
+    }
+}
---
+++ Single_Responsibility/InvoicePrinter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Responsibility
+{
+    public class InvoicePrinter
+    {
+        public void Print(Invoice invoice) {
+
+            // This class handles only the printing resposibility
+            Console.WriteLine($"Invoice {invoice.InvoiceId}: {invoice.CalculateTotal()}");
+        }
+    }
+}
